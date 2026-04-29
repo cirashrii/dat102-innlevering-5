935f2ec0ae88cd5c0f95b348b141aa821b4d6f7a
cirashrii <cirashrii@gmail.com>
2026-04-29 15:28:57 +0200

    haugsortering

--- src/oppgave17_1/Main.java
+++ src/oppgave17_1/Main.java
@@ -12,14 +12,31 @@ public class Main {
         Integer[] numbers = new Integer[10];
         Random random = new Random();
 
+        int k = 5;
+
         for(int i = 0; i<numbers.length;i++) {
             numbers[i] = random.nextInt(20);
         }
 
-        sorterVedInnsetting(numbers, 5);
-        //utvalgssortering(numbers, 5);
-//        Haug test = new Haug(numbers);
-//        test.toString();
+//        sorterVedInnsetting(numbers, 5);
+//        utvalgssortering(numbers, 5);
+
+        System.out.println("Vi spurte om de " + k + " minste tallene.");
+        System.out.println("Before: ");
+        for (Integer t : numbers) {
+            System.out.print(t + " ");
+        }
+
+        System.out.println();
+        System.out.println("After: ");
+
+        Haug test = new Haug();
+
+        test = test.finnKMinste(numbers, 5);
+
+        for (int i = 1; i <= test.getAntall(); i++) {
+            System.out.print(test.getHaug()[i] + " ");
+        }
 
 
     }
